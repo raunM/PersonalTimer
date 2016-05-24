296a04215b54e305379c4e3525fa2c9bceb7b042
Raun Mathur <raun.mathur@gmail.com>
2016-05-24 13:57:22 +0000

    updated db and routes

--- app/controllers/timelogs_controller.rb
+++ app/controllers/timelogs_controller.rb
@@ -1,16 +1,21 @@
 class TimelogsController < ApplicationController
     before_action :set_log, only: [:destroy]
+    before_action :authenticate_user!, except: [:home]
+    def home
+    end
+
     def index
         @time_logs = TimeLog.all
     end
     
     def new
         @timelog = TimeLog.new
+        #@timelog = current_user.time_logs.build(log_params)
     end
     
     def create
-        @timelog = TimeLog.new(log_params)
-    
+        #@timelog = TimeLog.new(log_params)
+        @timelog = current_user.time_logs.build(log_params)
         respond_to do |format|
           if @timelog.save
             format.html { redirect_to @timelog, notice: 'Post was successfully created.' }
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
     devise_for :users do
       get'users/sign_out'=>'devise/sessions#destroy'
     end
-    resources :timelogs
+    resources :users do
+      resources :timelog
+    end
     root 'timelogs#home'
 end
