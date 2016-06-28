7a2647366bb016fc66e07c2f0bfa98cc02e8243f
Raun Mathur <raun.mathur@gmail.com>
2016-06-28 15:01:33 +0000

    authenticate only on home

--- app/controllers/timelogs_controller.rb
+++ app/controllers/timelogs_controller.rb
@@ -1,58 +1,58 @@
 class TimelogsController < ApplicationController
-    before_action :set_log, only: [:destroy, :edit, :update]
-    before_action :authenticate_user!, only: [:index, :new]
-    def home
+  before_action :set_log, only: [:destroy, :edit, :update]
+  before_action :authenticate_user!, except: [:home]
+  def home
+  end
+
+  def index
+    @timelogs = current_user.timelogs.order("created_at DESC")
+  end
+
+  def new
+    @timelog = current_user.timelogs.build
+  end
+
+  def create
+    @timelog = current_user.timelogs.build(log_params)
+
+    if @timelog.save
+      redirect_to new_user_timelog_path
+    else
+      render action: :new
     end
+  end
 
-    def index
-        @timelogs = current_user.timelogs.order("created_at DESC")
+  def edit
+  end
+
+  def update
+    if @timelog.update(log_params)
+      redirect_to user_timelogs_path
+    else
+      render action: :new
     end
+  end
 
-    def new
-        @timelog = current_user.timelogs.build
+  def destroy
+    @timelog.destroy
+    respond_to do |format|
+      format.html { redirect_to user_timelogs_path, notice: 'Timelog was successfully destroyed.' }
+      format.json { head :no_content }
     end
+  end
 
-    def create
-        @timelog = current_user.timelogs.build(create_params)
+  private
 
-        if @timelog.save
-            redirect_to new_user_timelog_path
-        else
-            render action: :new
-        end
+  def set_log
+    @timelog = current_user.timelogs.find(params[:id])
+  end
+
+  def log_params
+    if params[:action] == "new"
+      params.require(:timelog).permit(:description, :timespent, :category)
+    else
+      params.require(:timelog).permit(:description, :category)
     end
-
-    def edit
-    end
-
-    def update
-        if @timelog.update(up_params)
-            redirect_to user_timelogs_path
-        else
-            render action: :new
-        end
-    end
-
-    def destroy
-        @timelog.destroy
-        respond_to do |format|
-            format.html { redirect_to user_timelogs_path, notice: 'Timelog was successfully destroyed.' }
-            format.json { head :no_content }
-        end
-    end
-
-    private
-
-        def set_log
-            @timelog = current_user.timelogs.find(params[:id])
-        end
-
-        def create_params
-          params.require(:timelog).permit(:description, :timespent, :category)
-        end
-
-        def up_params
-          params.require(:timelog).permit(:description, :category)
-        end
+  end
 
 end
